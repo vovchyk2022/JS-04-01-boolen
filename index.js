870826e1d93df4e6c668c44b79b36d218049f05d
Volodymyr Kapushchak <poshta070887@gmail.com>
2023-01-04 10:44:13 +0200

    Add operators

--- index.js
+++ index.js
@@ -1,54 +1,78 @@
 // alert(123)
 // window.alert(456)
 
-console.log(123);
-console.log(12.3);
-console.log('text');
-console.log(true);
-console.log(null);
-console.log(undefined);
+// console.log(123);
+// console.log(12.3);
+// console.log('text');
+// console.log(true);
+// console.log(null);
+// console.log(undefined);
 
-let userName = 'Brad';
-// let userAge; //оголошення
-// console.log(typeof userAge);
-// userAge=59; //ініціалізація
+// let userName = 'Brad';
+// // let userAge; //оголошення
+// // console.log(typeof userAge);
+// // userAge=59; //ініціалізація
 
-console.log(userName);
-// console.log(userAge);
+// console.log(userName);
+// // console.log(userAge);
 
-const userGender = 'male';
-const isUserMale = true;
-//userGender = 'female'; // erroy
+// const userGender = 'male';
+// const isUserMale = true;
+// //userGender = 'female'; // erroy
 
-const AMOUNT_DAYS_WEEK = 7; // - натуральна константа
-let number = 12;
+// const AMOUNT_DAYS_WEEK = 7; // - натуральна константа
+// let number = 12;
 
-{let number = 89;
-console.log(number);
-}
+// {let number = 89;
+// console.log(number);
+// }
+
+// alert()
+
+/*let number = 12;
+number = number + 5;
+console.log(number);*/
+
+//number += 5;
+
+//number = (number + 4) * 2;
+
+// let count = 1;
+// ++count;
+// count++;
+// console.log(count);
+
+//3
+// const number1 = 12;
+// const number2 = 3;
+
+// let result1 = number1 + number2; 
+// console.log('result1',result1);
+
+// let result2 = number1 / number2; 
+// console.log('result2',result2);
+
+// let result3 = number1 * number2; 
+// console.log('result3',result3);
+
+// let result4 = number1 % number2; 
+// console.log('result4',result4);
 
 
+// const number3 = '12';
+// const number4 = '3';
+
+// let result5 = number3 + number4; 
+// console.log('result5',result5);
 
 
+// let result = '12'*3+ +'4';
+// console.log('result',result);
 
+// let result6 = Number('12')*3 + '' + Number('4')*Number('bfbf');
+// console.log('result6',result6);
 
-let userAge = 25
-console.log('user age:', userAge);
-
-const isUserAdult = true;
-
-const userEmail = 'user@gmail.com';
-console.log('email:', userEmail);
-
-const nameProgrammingLanguage ='C++'
-console.log('programming language:',nameProgrammingLanguage);
-
-const isUserHavePet = false;
-
-const SEAZONS_OF_YEAR = '4'
-console.log('seasons of the year:', SEAZONS_OF_YEAR);
-
-
-
+console.log(true > false); //видає true
+console.log(true < false);//видає false
 
 
