2e8b7af33779d2de59a087cc9602bdb3ed0dbc86
Volodymyr Kapushchak <poshta070887@gmail.com>
2023-01-05 09:43:09 +0200

    Add 2 solves

--- index.js
+++ index.js
@@ -1,78 +1,31 @@
-// alert(123)
-// window.alert(456)
+    let number = 10;
+    
+    if (number < 0 || number% 2 !== 0 || number % 3 !== 0 || num % 6 !== 0) {
+        console.log('LOSE');
+      }  else {
+            console.log('WIN');
+        }
+    
 
-// console.log(123);
-// console.log(12.3);
-// console.log('text');
-// console.log(true);
-// console.log(null);
-// console.log(undefined);
-
-// let userName = 'Brad';
-// // let userAge; //оголошення
-// // console.log(typeof userAge);
-// // userAge=59; //ініціалізація
-
-// console.log(userName);
-// // console.log(userAge);
-
-// const userGender = 'male';
-// const isUserMale = true;
-// //userGender = 'female'; // erroy
-
-// const AMOUNT_DAYS_WEEK = 7; // - натуральна константа
-// let number = 12;
-
-// {let number = 89;
-// console.log(number);
-// }
-
-// alert()
-
-/*let number = 12;
-number = number + 5;
-console.log(number);*/
-
-//number += 5;
-
-//number = (number + 4) * 2;
-
-// let count = 1;
-// ++count;
-// count++;
-// console.log(count);
-
-//3
-// const number1 = 12;
-// const number2 = 3;
-
-// let result1 = number1 + number2; 
-// console.log('result1',result1);
-
-// let result2 = number1 / number2; 
-// console.log('result2',result2);
-
-// let result3 = number1 * number2; 
-// console.log('result3',result3);
-
-// let result4 = number1 % number2; 
-// console.log('result4',result4);
-
-
-// const number3 = '12';
-// const number4 = '3';
-
-// let result5 = number3 + number4; 
-// console.log('result5',result5);
-
-
-// let result = '12'*3+ +'4';
-// console.log('result',result);
-
-// let result6 = Number('12')*3 + '' + Number('4')*Number('bfbf');
-// console.log('result6',result6);
-
-console.log(true > false); //видає true
-console.log(true < false);//видає false
-
-
+    let num1 = 1;
+    let num2 = 0;
+    let res;
+    if (num1 > 0 && num2 > 0) {
+        res = num1 * num2;
+    } else {
+        if ( num1 === 0 && num2 === 0) {
+        res = 'NaN';
+        }
+     else {
+            if ( num1 < 0 || num2 < 0 ) {
+            res = num1 / num2;
+        } else {
+            if ( num1 === 0 ) {
+            res = num2;
+             } if (num2 === 0) {
+                res = num1; 
+             } 
+             }
+             }
+        }
+    console.log(res);
