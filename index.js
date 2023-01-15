2c37722a397ced3eb2f37d1bc98431d5544e3fc9
Volodymyr Kapushchak <poshta070887@gmail.com>
2023-01-15 13:18:09 +0200

    Add solves

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let number = 10;
+let number = 80;
 
-if (number < 0 || number % 2 !== 0 || number % 3 !== 0 || num % 6 !== 0) {
+if (number < 0 || number % 2 || number % 4 || number % 7) {
   console.log("LOSE");
 } else {
   console.log("WIN");
@@ -11,8 +11,9 @@ if (number < 0 || number % 2 !== 0 || number % 3 !== 0 || num % 6 !== 0) {
 // якщо одне зі значень 0 - то покласти в результат значення іншої змінної і вивести результат,
 // якщо обидва нулі - то покласти в результат NaN і вивести результат.
 
+// // Варіант1
 let num1 = 0;
-let num2 = 2;
+let num2 = -5;
 let res;
 if ((num1 > 0 && num2 > 0) || (num1 < 0 && num2 < 0)) {
   res = num1 * num2;
@@ -32,5 +33,57 @@ if ((num1 > 0 && num2 > 0) || (num1 < 0 && num2 < 0)) {
     }
   }
 }
+console.log(res);
 
+// Варіант2
+// debugger
+if ((num1 > 0 && num2 > 0) || (num1 < 0 && num2 < 0)) {
+  res = num1 * num2;
+} else {
+  res = num1 / num2;
+}
+if (num1 === 0 && num2 === 0) {
+  res = NaN;
+} else {
+  if (num1 === 0) {
+    res = num2;
+  }
+  if (num2 === 0) {
+    res = num1;
+  }
+}
+console.log(res);
+
+// Варіант3
+// debugger
+if ((num1 > 0 && num2 > 0) || (num1 < 0 && num2 < 0)) {
+  res = num1 * num2;
+} else {
+  res = num1 / num2;
+}
+if (num1 === 0 && num2 === 0) {
+  res = NaN;
+} else if (num1 === 0 || num2 === 0) {
+res = num1 || num2;
+}
+console.log(res);
+
+
+// Варіант4 - неправильно
+// debugger;
+if (num1 === 0 && num2 === 0) {
+  res = NaN;
+} else {
+  if (num1 === 0) {
+    res = num2;
+  }
+  if (num2 === 0) {
+    res = num1;
+  }
+}
+if ((num1 > 0 && num2 > 0) || (num1 < 0 && num2 < 0)) {
+  res = num1 * num2;
+} else {
+  res = num1 / num2;
+}
 console.log(res);
